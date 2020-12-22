dbffbf9ddb6ad8c5aa8b6c5ec386f5f56d802363
Stepan <trofimovjava@gmail.com>
2020-12-22 03:35:25 +0300

    Fix include paths

--- Source/UserInterfacePack/Public/Libraries/UserInterfaceHelper.h
+++ Source/UserInterfacePack/Public/Libraries/UserInterfaceHelper.h
@@ -5,6 +5,7 @@
 #include "CoreMinimal.h"
 #include "Kismet/BlueprintFunctionLibrary.h"
 #include "UserInterfaceStructs.h"
+#include "Styling/SlateTypes.h"
 #include "UserInterfaceHelper.generated.h"
 
 class USizeBox;
